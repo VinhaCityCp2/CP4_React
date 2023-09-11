e33f99a9973719e6d495952c91917b2fcf8d4bba
Henrique Lima <127214958+limas0209@users.noreply.github.com>
2023-09-11 11:56:58 -0300

    nav1

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,13 +1,16 @@
 import { } from 'react'
 import { NavLink } from 'react-router-dom'
 
+
 function Nav(){
+    
     return(
         <>
         <NavLink to='Home'> Home </NavLink>
         <NavLink to='Produtos'> Produtos </NavLink>
         <NavLink to='Sobre'> Sobre </NavLink>
         </>
+        
     )
 }
 
