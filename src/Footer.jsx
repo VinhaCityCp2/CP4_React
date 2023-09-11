5db33fee003d37e9c57ad8e0cbd96dd9a6a73516
Ian Xavier Kuraoka <110792458+ianXkuraoka@users.noreply.github.com>
2023-09-11 11:52:49 -0300

    syntax_fix1

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,10 +2,12 @@ import { } from 'react'
 
 function Footer(){
     return(
+
         <>
         <p>Telefone: 11943347167</p>
         <p>Email: Fazitoguilherme@gmail.com</p>
         </>
+        
     )
 }
 
